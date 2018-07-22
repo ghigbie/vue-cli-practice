7a33313d54204cbaaf0fb21208d765dbc14e950a
ghigbie <gehigbie@gmail.com>
2018-07-22 17:10:00 -0600

    Add v-for directive

--- src/components/AllFriends.vue
+++ src/components/AllFriends.vue
@@ -1,6 +1,9 @@
 <template>
     <div id="all-friends">
         <h2>All Friends</h2>
+        <div v-for="friend in friends">
+            <span> {{ friend.name }} </span>
+        </div>
     </div>
 </template>
 
@@ -9,7 +12,12 @@ export default {
     name: 'AllFriends',
     data(){
         return {
-            
+            friends: [
+                {name: 'Mario', online: true},
+                {name: 'Luigi', online: false},
+                {name: 'Toad', online: true},
+                {name: 'Bowser', online: false}
+            ]
         }
     }
 }
