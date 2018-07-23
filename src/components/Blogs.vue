4c5256cb1e0b4da2b8b293849ce0b4cade0ed59f
ghigbie <gehigbie@gmail.com>
2018-07-22 18:53:48 -0600

    Add computeed property and method

--- src/components/Blogs.vue
+++ src/components/Blogs.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="blogs">
         <h2>Blogs</h2>
+        <input type="text" v-model="searchTerm"/>
         <div v-for="post in posts"
              :key="post.id">
             <h3>{{ post.title }}</h3>
-            <p>{{ post.body }}</p>
+            <p>{{ post.body | snippet }}</p>
         </div>
+        <div v-if="err">
+            {{ errMessage }}
+        </div>  
     </div>
 </template>
 
@@ -16,16 +20,29 @@ export default {
     name: 'Blogs',
     data() {
         return {
-            posts: []
+            posts: [],
+            err: '',
+            errMessage: 'There was an error. Please try again',
+            searchTerm: ''
         };
     },
     methods: {
 
     },
+    computed: {
+        filteredPosts() {
+           return  this.posts.filter(post => {
+               post.body === this.searchTerm;
+           })
+        }
+    },
     created(){
         axios.get('https://jsonplaceholder.typicode.com/posts/')
             .then(response => {
                 this.posts = response.data;
+            }).catch(err => {
+                console.log(err);
+                this.err = err;
             });
     }
 }
